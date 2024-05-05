2ea3a3a57a705b35a49c07637643354a17ba47e1
professorthiagoxavier <professorthiagoxavier@gmail.com>
2024-05-05 09:45:09 -0300

    aulas: 1, 2, 3 e 4

---
+++ src/app/componets/first-component/first-component.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-first-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.css'
+})
+export class FirstComponentComponent {
+  name:string = "Thiago Xavier"
+  age:number = 40
+  job:string = "Engenheiro líder"
+  hobbies = ["futebol", "bike", "músicas"]
+  car = {marca: "Ford", modelo:"fiesta"}
+  img = '../../assets/img/angular.jpg'  
+
+
+}
