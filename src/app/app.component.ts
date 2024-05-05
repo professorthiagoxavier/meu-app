8aad8caec922de33f7bbab7a9973cd9916cbbd27
professorthiagoxavier <professorthiagoxavier@gmail.com>
2024-05-05 10:34:09 -0300

    control flow condicional if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './componets/first-component/first-component.component';
+import { ControlFlowComponent } from './components/control-flow/control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent],
+  imports: [RouterOutlet, FirstComponentComponent, ControlFlowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/control-flow/control-flow.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-flow',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './control-flow.component.html',
+  styleUrl: './control-flow.component.css'
+})
+export class ControlFlowComponent {
+
+  mostrar:boolean = true
+  color:string = "red"
+
+  constructor(){
+
+  }
+
+}
